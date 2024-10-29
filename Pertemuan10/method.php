96ed26a860e1da9eb50207bbc200b36fcef69360
Muhammad Agus Rizal <rzllas25@gmail.com>
2024-10-29 14:37:26 +0700

    komit pertama

---
+++ Pertemuan10/method.php
@@ -0,0 +1,12 @@
+<?php
+class Kue {
+    var $bahan = ['Tepung', 'Gula', 'Telur'];
+    var $bentuk;
+    function memasak_kue ([parameter]) {
+      
+    }
+    function membungkusKue ([parameter]) {
+       
+    }
+}
+?>
---
+++ Pertemuan10/parameter.php
@@ -0,0 +1,26 @@
+<?php
+class Kue {
+    var $bahan = ['Tepung', 'Gula', 'Telur'];
+    var $bentuk;
+
+    function memasak_kue($jenis_kue) {
+        echo "Memasak kue $jenis_kue dengan bahan: " . implode(", ", $this->bahan) . ".<br>";
+    }
+
+    function membungkusKue($jenis_bungkus) {
+        echo "Membungkus kue dengan $jenis_bungkus.<br>";
+    }
+}
+
+$kue_bolu = new Kue();
+$kue_bolu->memasak_kue("Bolu");
+$kue_bolu->membungkusKue("kotak");
+
+$kue_donat = new Kue();
+$kue_donat->memasak_kue("Donat");
+$kue_donat->membungkusKue("plastik");
+
+$kue_gethuk = new Kue();
+$kue_gethuk->memasak_kue("Gethuk");
+$kue_gethuk->membungkusKue("daun pisang");
+?>
